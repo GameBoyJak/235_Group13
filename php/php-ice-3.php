c10387d5a74938cfda1a7296f30a5281c4099edd
dlc2787 <54549285+dlc2787@users.noreply.github.com>
2019-10-15 19:18:45 -0400

    Finished ICE-3
    finished ICE-3

--- php/php-ice-3.php
+++ php/php-ice-3.php
@@ -4,14 +4,15 @@
 <meta charset="UTF-8">
 <title>PhP3</title>
 </head><body>
+<h1>About this page and you</h1>
 <?php
 
-foreach($_SERVER as $key=>$value){
-	echo "<p>$key=>$value</p>";
-}
-echo $_SERVER['SERVER_ADDR'];
-echo $_SERVER['SERVER_SOFTWARE'];
-echo $_SERVER['REQUEST_TIME'];
+echo "<p>Server Name: " . $_SERVER['SERVER_NAME'] . "</p>";
+echo "<p>Your Address: " . $_SERVER['REMOTE_ADDR'] . "</p>";
+echo "<p>Your Browser: " . $_SERVER['HTTP_USER_AGENT'] . "</p>";
+echo "<p>Server Address: " . $_SERVER['SERVER_ADDR'] . "</p>";
+echo "<p>Server Software: " . $_SERVER['SERVER_SOFTWARE'] . "</p>";
+echo "<p>Server Time: " . date("m/d/Y G:i:s", $_SERVER['REQUEST_TIME']) . "</p>";
 
 ?>
 </body></html>
